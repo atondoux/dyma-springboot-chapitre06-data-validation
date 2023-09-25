b344b7ecd91dca3440691b0590d50c1faef1fbba
Aurélien <atondoux@gmail.com>
2023-09-25 21:44:49 +0200

    :sparkles: data validation

--- src/main/java/com/dyma/tennis/web/PlayerController.java
+++ src/main/java/com/dyma/tennis/web/PlayerController.java
@@ -9,6 +9,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -53,7 +54,7 @@ public class PlayerController {
 
     })
     @PostMapping
-    public Player createPlayer(@RequestBody Player player) {
+    public Player createPlayer(@RequestBody @Valid Player player) {
         return player;
     }
 
@@ -65,7 +66,7 @@ public class PlayerController {
 
     })
     @PutMapping
-    public Player updatePlayer(@RequestBody Player player) {
+    public Player updatePlayer(@RequestBody @Valid Player player) {
         return player;
     }
 
